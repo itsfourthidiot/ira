fe095e850861d6282c4827bf8b7a76c75f425d2d
Gauri Pandharinath Bodke <gauribodke95@gmail.com>
2022-03-02 17:55:49 -0500

    course enroll api

--- backend/model.go
+++ backend/model.go
@@ -23,9 +23,9 @@ func (i Instructor) getUserDetails() string {
 
 type Student struct {
 	gorm.Model
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"-"`
-	// Enrollments []Enrollment
+	Email       string `json:"email" gorm:"unique"`
+	Password    string `json:"-"`
+	Enrollments []Enrollment
 }
 
 func (s Student) getUserDetails() string {
@@ -39,12 +39,12 @@ type Course struct {
 	IsPublished  bool       `json:"isPublished" gorm:"default:false"`
 	PublishedAt  *time.Time `json:"publishedAt"`
 	InstructorID uint       `json:"instructorId"`
-	// Enrollments  []Enrollment
+	Enrollments  []Enrollment
 }
 
-// type Enrollment struct {
-// 	gorm.Model
-// 	StudentID uint `json:"studentId" gorm:"primaryKey"`
-// 	CourseID  uint `json:"courseId" gorm:"primaryKey"`
-// 	Enrolled_on *time.Time
-// }
+type Enrollment struct {
+	gorm.Model
+	StudentID   uint `json:"studentId" gorm:"primaryKey"`
+	CourseID    uint `json:"courseId" gorm:"primaryKey"`
+	Enrolled_on *time.Time
+}
