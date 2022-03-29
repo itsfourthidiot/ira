4b81bc000705eca1a438f25d0ee09392b1b14216
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-29 16:13:12 -0400

    Add APIs to access course curriculum

--- backend/model.go
+++ backend/model.go
@@ -7,41 +7,41 @@ import (
 )
 
 type User interface {
-	getUserDetails() string
+	getUserDetails() (string, string)
 }
 
 type Instructor struct {
 	gorm.Model
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"-"`
-	Courses  []Course
+	Email    string   `json:"email" gorm:"unique"`
+	Password string   `json:"-"`
+	Courses  []Course `json:"courses"`
 }
 
-func (i Instructor) getUserDetails() string {
-	return i.Email
+func (i Instructor) getUserDetails() (string, string) {
+	return i.Email, "instructor"
 }
 
 type Student struct {
 	gorm.Model
-	Email       string `json:"email" gorm:"unique"`
-	Password    string `json:"-"`
-	Enrollments []Enrollment
+	Email       string       `json:"email" gorm:"unique"`
+	Password    string       `json:"-"`
+	Enrollments []Enrollment `json:"enrollements"`
 	Scores      []Score
 }
 
-func (s Student) getUserDetails() string {
-	return s.Email
+func (s Student) getUserDetails() (string, string) {
+	return s.Email, "student"
 }
 
 type Course struct {
 	gorm.Model
-	Title        string     `json:"title" gorm:"not null"`
-	Description  *string    `json:"description"`
-	IsPublished  bool       `json:"isPublished" gorm:"default:false"`
-	PublishedAt  *time.Time `json:"publishedAt"`
-	InstructorID uint       `json:"instructorId"`
-	Enrollments  []Enrollment
-	Modules      []Module
+	Title        string       `json:"title" gorm:"not null"`
+	Description  *string      `json:"description"`
+	IsPublished  bool         `json:"isPublished" gorm:"default:false"`
+	PublishedAt  *time.Time   `json:"publishedAt"`
+	InstructorID uint         `json:"instructorId"`
+	Enrollments  []Enrollment `json:"-"`
+	Modules      []Module     `json:"modules"`
 }
 
 type Enrollment struct {
@@ -57,14 +57,14 @@ type Module struct {
 	Type      string `json:"type"`
 	IsPrivate bool   `json:"isPrivate"`
 	CourseID  uint   `json:"courseId"`
-	Video     Video
-	Quiz      Quiz
+	Video     Video  `json:"video"`
+	Quiz      Quiz   `json:"quiz"`
 }
 
 type Video struct {
 	gorm.Model
-	Url      string `json:"url"`
-	ModuleID uint   `json:"moduleId"`
+	Key      *string `json:"key"`
+	ModuleID uint    `json:"moduleId"`
 }
 type Quiz struct {
 	gorm.Model
@@ -74,9 +74,9 @@ type Quiz struct {
 }
 type Question struct {
 	gorm.Model
-	QuizID  uint   `json:"quizId"`
-	Content string `json:"content" gorm:"not null"`
-	Options []Option
+	QuizID  uint     `json:"quizId"`
+	Content string   `json:"content" gorm:"not null"`
+	Options []Option `json:"options"`
 }
 
 type Option struct {
