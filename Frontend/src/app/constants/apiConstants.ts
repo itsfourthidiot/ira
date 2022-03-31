92793aa2e337c529359bafd8e08285324e8b44f7
prasadhbk222 <phadkar@ufl.edu>
2022-03-31 19:24:39 -0400

    synchronizing api format

--- Frontend/src/app/constants/apiConstants.ts
+++ Frontend/src/app/constants/apiConstants.ts
@@ -1,11 +1,14 @@
 export const apiUrls = {
     baseUrl : 'http://10.20.106.43:8080',
-    updateDescription : '/course/updateDescription',
+    updateDescription : 'instructor/course/<courseId>/description',
     createCourses : '/instructor/course',
     instrCourses : '/instructor/courses',
     uploadVideo : '/instructor/course/<courseId>/module/video',
     getAllCourses : '/courses',
     uploadQuiz : '/instructor/course/<courseId>/module/quiz',
     getCourseDetails : '/courses/<courseId>',
-    publishCourse : 'instructor/course/<courseId>/publish'
+    publishCourse : 'instructor/course/<courseId>/publish',
+    studentEnroll : 'student/course/<courseId>/enroll',
+    checkEnroll : 'student/course/<courseId>/enroll',
+    getCourseDescription : 'instructor/course/<courseId>/description'
 }
--- Frontend/src/assets/data/allCourses.ts
+++ Frontend/src/assets/data/allCourses.ts
@@ -13,6 +13,18 @@ export const allCourses: any =  {
             "publishedAt": null,
             "instructorId": 7,
             "modules": null
+        },
+        {
+            "ID": 2,
+            "CreatedAt": "2022-03-29T23:11:30.510395975-04:00",
+            "UpdatedAt": "2022-03-29T23:11:30.80831566-04:00",
+            "DeletedAt": null,
+            "title": "Shirtttt directional 1080p",
+            "description": "Aut sed sed ipsum ut nihil. Aut ipsum nemo. Itaque cum quam officia tempore soluta. Aut sint et ullam sunt amet exercitationem est est. Ut beatae quidem molestiae beatae. Magnam quisquam fugiat nesciunt.\n \rQuo qui nisi reiciendis. Minus iusto earum modi ut odio. Maxime distinctio hic molestiae ratione vitae.\n \rNulla non officiis eaque ut. Sed facere tempore ut at voluptatem minima autem ipsam earum. Delectus est non sunt qui. Non deleniti ratione et architecto molestias quisquam nemo. Exercitationem corrupti excepturi veniam ullam iste inventore.",
+            "isPublished": true,
+            "publishedAt": null,
+            "instructorId": 7,
+            "modules": null
         }
     ]
 }
