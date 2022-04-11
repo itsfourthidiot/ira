a4415e95a14721519fe5fc16d35334d2595766c2
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-04-11 17:28:57 -0400

    Add continuous deployment support to aws ec2 instance

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/ubuntu/ira/Frontend/
+npm install
+./node_modules/.bin/ng build
+cd /home/ubuntu/ira/Backend/
+go build
+echo "SECRET_KEY=$SECRET_KEY" >> .env
+echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
+echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
+echo "AWS_REGION=$AWS_REGION" >> .env
+echo "AWS_BUCKET=$AWS_BUCKET" >> .env
+./ira
