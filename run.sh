cc66506af40bd318f9f4c8b45ab26d1aa8e62c40
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-04-11 17:54:23 -0400

    Update deployment scripts

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu/ira/Frontend/
 npm install
 ./node_modules/.bin/ng build
-cd /home/ubuntu/ira/Backend/
+cd /home/ubuntu/ira/backend/
 go build
 echo "SECRET_KEY=$SECRET_KEY" >> .env
 echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
