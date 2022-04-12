42da5f27f82026e5abcdf344cdf8a33dd1f8e53f
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-04-11 20:05:49 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export PATH=$PATH:/usr/local/go/bin
 cd /home/ubuntu/ira/Frontend/
 npm install
 ./node_modules/.bin/ng build
